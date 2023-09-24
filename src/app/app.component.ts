656d0cac94b11f8cb556ee904b578ba787a5f65b
Guilherme de Alacoc Aquino <guilhermealacoc@gmail.com>
2023-09-24 20:08:57 -0300

    estudo de outputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,23 @@ import { Component, OnInit } from '@angular/core';
     <br />
     <button (click)="add()">Adicionar</button>
     <br />
-    <app-output></app-output>
+    <ng-template [ngIf]="getDados">
+      <h1>{{ getDados.nome }}</h1>
+      <h1>{{ getDados.idade }}</h1>
+    </ng-template>
+    <app-output (enviarDados)="setDados($event)"></app-output>
   `,
 })
 export class AppComponent implements OnInit {
   constructor() {}
+  public getDados: { nome: string; idade: number } | undefined;
   public addValue: number = 0;
   public add() {
     this.addValue++;
   }
   ngOnInit(): void {}
+
+  public setDados(event: { nome: string; idade: number }) {
+    this.getDados = event;
+  }
 }
